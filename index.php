ec57a419f95216d0b8f44c2e135b3a1c60602009
Hazart29 <misbakhul2904@gmail.com>
2022-01-23 18:03:08 +0700

    update commit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Prototype redesign satria app</title>
     <script src="https://cdn.tailwindcss.com"></script>
 
 </head>
@@ -13,6 +13,11 @@
 <body>
     <div>
         <iframe class="w-full h-screen border-solid border-1 border-black" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FTX47Uc9bcOrVoka40Q7ep7%2FPrototype-redesign-satria%3Fnode-id%3D2%253A14%26scaling%3Dscale-down%26page-id%3D0%253A1%26starting-point-node-id%3D2%253A14" allowfullscreen></iframe>
+        <a href="https://github.com/hazart29/protorypesatria">
+        <div class="flex fixed w-14 h-14 justify-center items-center bottom-0 right-0 mr-10 mb-10 bg-red-500 rounded-full text-center">
+            <div>rate</div>
+        </div>
+        </a>
     </div>
 </body>
 
