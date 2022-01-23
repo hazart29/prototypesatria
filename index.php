ae4a6aa0470dc4d7f72f99671ab00d6bc220944f
Hazart29 <misbakhul2904@gmail.com>
2022-01-23 17:06:46 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+
+<body>
+    <div>
+        <iframe class="w-full h-screen border-solid border-1 border-black" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FTX47Uc9bcOrVoka40Q7ep7%2FPrototype-redesign-satria%3Fnode-id%3D2%253A14%26scaling%3Dscale-down%26page-id%3D0%253A1%26starting-point-node-id%3D2%253A14" allowfullscreen></iframe>
+    </div>
+</body>
+
+</html>
